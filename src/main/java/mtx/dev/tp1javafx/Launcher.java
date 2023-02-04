3cbd9f91cd8ee79b985f849a8a948669c07c4386
ArthurMTX <itsmitralyx@gmail.com>
2023-02-04 17:57:50 +0100

    commit de zinzin

---
+++ src/main/java/mtx/dev/tp1javafx/Launcher.java
@@ -0,0 +1,14 @@
+package mtx.dev.tp1javafx;
+
+/**
+ * Launcher de l'application
+ */
+public class Launcher {
+    /**
+     * Lance l'application
+     * @param args Arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
